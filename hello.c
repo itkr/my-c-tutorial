f891738e84fedb1af6f81a65e50eaf4b1821b374
itkr <itkrst@gmail.com>
2017-11-07 01:33:05 +0900

    First commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int getInt();
+
+int main(int argc, char *argv[]) {
+    int i;
+    printf("Hello, World\n");
+    printf("argc = %d\n", argc);
+    for (i = 0; i < argc; i++) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    printf("%d\n", getInt());
+    exit(0);
+}
+
+int getInt() {
+    return 1;
+}
