301d1a02c5cf2f6e200b120ebf45102a38781e20
itkr <itkrst@gmail.com>
2017-11-21 19:10:36 +0900

    catの実装

--- hello.c
+++ hello.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int getInt();
 
 int main(int argc, char *argv[]) {
-    int i;
-    printf("Hello, World\n");
-    printf("argc = %d\n", argc);
-    for (i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-    printf("%d\n", getInt());
-    exit(0);
+  int i;
+  printf("Hello, World\n");
+  printf("argc = %d\n", argc);
+  for (i = 0; i < argc; i++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  printf("%d\n", getInt());
+  exit(0);
 }
 
 int getInt() {
-    return 1;
+    return 0;
 }
